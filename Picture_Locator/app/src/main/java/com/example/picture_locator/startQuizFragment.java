46d6984eb04b813be2304e92107a025d642b5333
YutengMei <yutengmei@gmail.com>
2019-05-15 15:03:06 -0400

    Update

--- Picture_Locator/app/src/main/java/com/example/picture_locator/startQuizFragment.java
+++ Picture_Locator/app/src/main/java/com/example/picture_locator/startQuizFragment.java
@@ -4,14 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 public class startQuizFragment extends Fragment {
+
     public startQuizFragment() {
 
     }
@@ -29,6 +33,11 @@ public class startQuizFragment extends Fragment {
                 startActivity(dartmouthqQuiz);
             }
         });
+
+
+
         return v;
     }
+
+
 }
