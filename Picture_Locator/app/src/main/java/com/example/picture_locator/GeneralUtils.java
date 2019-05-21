7c0eeeab3b5494d4fb5e9c6fbf4f38e9a7446487
Rodrigo Cavero Blades <rodrigo.a.cavero.blades.21@dartmouth.edu>
2019-05-21 15:40:38 -0400

    fixed stuff

--- Picture_Locator/app/src/main/java/com/example/picture_locator/GeneralUtils.java
+++ Picture_Locator/app/src/main/java/com/example/picture_locator/GeneralUtils.java
@@ -1,12 +1,16 @@
 package com.example.picture_locator;
 
 import androidx.exifinterface.media.ExifInterface;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
 import com.google.android.gms.maps.model.LatLng;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class GeneralUtils {
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public static LatLng getLatLngFromUri(FileInputStream image) throws IOException {
         float[] arr = new float[2];
         ExifInterface exif = new ExifInterface(image);
